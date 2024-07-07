63eaa41efbb9a002743f8cbd945ed607119db88b
JOwithS <jo.ureta@duocuc.cl>
2024-07-07 02:26:45 -0400

    cambiosss

---
+++ src/app/persona/persona.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+/*import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-persona',
+  templateUrl: './persona.page.html',
+  styleUrls: ['./persona.page.scss'],
+})
+export class PersonaPage implements OnInit {
+  listado = [];
+
+  constructor(private api: ApiService) { }
+
+  ngOnInit() {
+  }
+  listar()
+  {
+    this.api.getUsers();
+    this.listado = this.api.listado;
+  }
+
+}
+*/
